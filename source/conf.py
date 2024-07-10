c99c350ecb401f6910a52c2452459f4f2f14fc46
kzhoujlu <az351250965@gamil.com>
2024-07-11 07:56:49 +0800

    all

--- source/conf.py
+++ source/conf.py
@@ -27,5 +27,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
-html_theme = 'press'
+#html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
